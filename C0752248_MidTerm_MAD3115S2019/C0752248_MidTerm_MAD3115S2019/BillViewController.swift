1ec96ce80300627f57c9567096aa89737e32594e
um-31 <udaymahajan97@gmail.com>
2019-07-12 00:11:08 -0400

    [Udhay] login working and tested

--- C0752248_MidTerm_MAD3115S2019/C0752248_MidTerm_MAD3115S2019/BillViewController.swift
+++ C0752248_MidTerm_MAD3115S2019/C0752248_MidTerm_MAD3115S2019/BillViewController.swift
@@ -12,9 +12,12 @@ class BillViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 
         // Do any additional setup after loading the view.
     }
+    
+    
     
 
     /*
--- C0752248_MidTerm_MAD3115S2019/C0752248_MidTerm_MAD3115S2019/ViewController.swift
+++ C0752248_MidTerm_MAD3115S2019/C0752248_MidTerm_MAD3115S2019/ViewController.swift
@@ -13,41 +13,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtUsername: UITextField!
     @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var switchRemember: UISwitch!
+    var userList = [String:String]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        readFromUsers()
         // Do any additional setup after loading the view.
     }
-    private func getRememberMeValues() {
-        let userDefault = UserDefaults.standard
-        if let email = userDefault.string(forKey: "userEmail") {
-            txtUsername.text = email
-            if let pwd = userDefault.string(forKey: "userPassword") {
-                txtPassword.text = pwd
-            }
+    func readFromUsers() {
+        if let bundelPath = Bundle.main.path(forResource: "Users", ofType: "plist") {
+            let dictionary = NSMutableDictionary(contentsOfFile: bundelPath)
+            
+            userList = (dictionary!["Users"] as! NSDictionary) as! [String : String]
+            print(userList["admin1@gmail.com"]!)
+            
         }
     }
+//    private func getRememberMeValues() {
+//        let userDefault = UserDefaults.standard
+//        if let email = userDefault.string(forKey: "userEmail") {
+//            txtUsername.text = email
+//            if let pwd = userDefault.string(forKey: "userPassword") {
+//                txtPassword.text = pwd
+//            }
+//        }
+//    }
     
     @IBAction func btnLogin(_ sender: UIBarButtonItem) {
-        if self.txtUsername.text == "admin@gmail.com" && self.txtPassword.text == "123456"
-        {
-            let userDefault = UserDefaults.standard
-            let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-            let  userVC = storyBoard.instantiateViewController(withIdentifier: "BillListIdentifier") as! BillViewController
-            self.present(userVC, animated: true, completion: nil)
-            
-            if switchRemember.isOn{
-                userDefault.setValue(txtUsername.text, forKey: "userEmail")
-                userDefault.set(txtPassword.text, forKey: "userPassword")
+        checkLogin()
+    }
+    func checkLogin() {
+        for (k,v) in userList {
+            if self.txtUsername.text == k && self.txtPassword.text == v
+            {
+                let userDefault = UserDefaults.standard
+                let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+                let  userVC = storyBoard.instantiateViewController(withIdentifier: "BillListIdentifier") as! BillViewController
+                self.present(userVC, animated: true, completion: nil)
+                
+                if switchRemember.isOn{
+                    userDefault.setValue(txtUsername.text, forKey: "userEmail")
+                    userDefault.set(txtPassword.text, forKey: "userPassword")
+                }else {
+                    userDefault.removeObject(forKey: "userEmail")
+                    userDefault.removeObject(forKey: "userPassword")
+                }
             }else {
-                userDefault.removeObject(forKey: "userEmail")
-                userDefault.removeObject(forKey: "userPassword")
+                let alert = UIAlertController(title: "Error", message: "Invalid Username or Password", preferredStyle: .alert)
+                let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
             }
-        }else {
-            let alert = UIAlertController(title: "Error", message: "Invalid Username or Password", preferredStyle: .alert)
-            let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
-            alert.addAction(okButton)
-            self.present(alert, animated: true)
         }
     }
     @IBAction func unWindLogoutFromAnyScreen(storyboardSegue: UIStoryboardSegue) {
